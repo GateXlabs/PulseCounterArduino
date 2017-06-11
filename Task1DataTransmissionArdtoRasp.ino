c419fd25b3ea722e005a27b59825f0a9d458f2c7
moataz94 <moataz.9994@gmail.com>
2017-06-11 16:00:59 +0400

    Send string from arduino to raspberry

---
+++ Task1DataTransmissionArdtoRasp.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+int address=1;
+String txt = "ARD123";
+
+void setup() {
+  // put your setup code here, to run once:
+     Wire.begin(address);
+     Wire.onRequest(sendMessage);
+}
+void loop() {}
+
+void sendMessage()
+{
+    Wire.write("ARD123"); 
+}
+
---
+++ Task1DataTransmissionArdtoRasp.py
@@ -0,0 +1,18 @@
+import smbus
+import time
+bus = smbus.SMBus(1)
+address = 1
+count = 0
+string=""
+def readNumber():
+         number2 = bus.read_i2c_block_data(1,0,7)
+         return number2
+
+
+while  True:
+        number=readNumber()
+        for i in range(len(number)):
+            string += chr(number[i])
+        count = number
+        print(string)
+     
